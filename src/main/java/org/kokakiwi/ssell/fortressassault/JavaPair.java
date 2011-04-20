d3f1fba9be849632714645f178e0ff3bdfbccfa1
Koka El Kiwi <admin@kokabsolu.com>
2011-04-20 21:11:33 +0200

    Personal changes for my own server.

--- src/main/java/org/kokakiwi/ssell/fortressassault/JavaPair.java
+++ src/main/java/org/kokakiwi/ssell/fortressassault/JavaPair.java
@@ -1,17 +1,15 @@
-package ssell.FortressAssault;
+package org.kokakiwi.ssell.fortressassault;
 
 /**
  * An implementation of the STL Pair from C++
  * 
  * @author ssell
  */
-public class JavaPair< T, S > 
-{
+public class JavaPair<T, S> {
 	public T first;
 	public S second;
-	
-	JavaPair( T first, S second )
-	{
+
+	JavaPair(T first, S second) {
 		this.first = first;
 		this.second = second;
 	}
